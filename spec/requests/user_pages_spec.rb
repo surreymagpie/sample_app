d12cc3140a035703f60927bbd3169cf70072e84d
surreymagpie <surreymagpie@yahoo.co.uk>
2014-01-01 22:11:24 +0000

    Added user profile page with gravatar

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,12 @@ describe "User pages" do
     it { should have_selector('h1', 'Sign Up') }
     it { should have_title(full_title('Sign Up')) }
   end
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 end
