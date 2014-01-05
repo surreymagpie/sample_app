b115c64c309f41948b48ff36483b46bdec627e0f
surreymagpie <surreymagpie@yahoo.co.uk>
2014-01-05 17:03:45 +0000

    Changed signin form template using form_tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    #render 'new'
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
       # Sign in and redirect to user profile page
       sign_in user
       redirect_to user
