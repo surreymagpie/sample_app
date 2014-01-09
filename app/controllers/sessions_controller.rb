dbc503bd165435bcb2957605933fae5b8530261b
surreymagpie <surreymagpie@yahoo.co.uk>
2014-01-09 08:16:21 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       # Sign in and redirect to user profile page
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       # Send error message and render the form again
       flash.now[:danger] = "Incorrect email/password combination"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update] 
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   
   def show
     @user = User.find(params[:id])
@@ -21,11 +27,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       #handle successful update
       flash[:success] = "Profile updated"
@@ -34,6 +38,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
   
   private
   
@@ -41,8 +51,22 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+    # BEFORE FILTERS
     def signed_in_user
-      flash[:warning] = "Please sign in"
-      redirect_to signin_url unless signed_in?
+      unless signed_in?
+        store_location
+        flash[:warning] = "Please sign in"
+        redirect_to signin_url
+      end      
     end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
+  
 end
